124ce566e14137862464e0e904c172a3b8b582db
Alexander Penkin <sss13594@gmail.com>
2018-04-23 14:54:48 +0500

    seq ppo

--- layer_norm.py
+++ layer_norm.py
@@ -1,33 +1,12 @@
 import torch
 import torch.nn as nn
-from torch.nn.modules.batchnorm import _BatchNorm
+from optfn.group_norm import _GroupNorm
 import torch.nn.functional as F
 
 
-class _LayerNorm(_BatchNorm):
+class _LayerNorm(_GroupNorm):
     def __init__(self, num_features, eps=1e-5, affine=True):
-        super().__init__(num_features, eps, 0.1, affine)
-        self.running_mean = torch.zeros(1)
-        self.running_var = torch.ones(1)
-
-    def forward(self, input):
-        b, c = input.size(0), input.size(1)
-
-        # Apply instance norm
-        input_reshaped = input.transpose(0, 1).contiguous().view(c, -1)
-
-        # Repeat stored stats and affine transform params
-        running_mean = self.running_mean.repeat(input_reshaped.shape[1])
-        running_var = self.running_var.repeat(input_reshaped.shape[1])
-
-        out = F.batch_norm(
-            input_reshaped, running_mean, running_var, None, None,
-            True, self.momentum, self.eps)
-
-        out = out.view(c, b, -1).transpose(0, 1)
-        out = out.mul(self.weight.unsqueeze(-1)).add_(self.bias.unsqueeze(-1))
-
-        return out.view_as(input)
+        super().__init__(num_features, 1, eps, affine)
 
 
 class LayerNorm1d(_LayerNorm):
--- rnn/dlstm.py
+++ rnn/dlstm.py
@@ -3,10 +3,11 @@ from torch.autograd import Variable
 import torch
 from optfn.layer_norm import LayerNorm1d
 from optfn.sigmoid_pow import sigmoid_pow
+from optfn.swish import Swish
 
 
 class DLSTM(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers=3, layer_norm=True, activation=nn.ReLU):
+    def __init__(self, input_size, hidden_size, num_layers=3, layer_norm=True, activation=nn.ELU):
         super().__init__()
         self.hidden_size = hidden_size
         self.num_layers = num_layers
@@ -14,28 +15,35 @@ class DLSTM(nn.Module):
 
         layers = []
         for layer_idx in range(num_layers):
+            last_layer = layer_idx == num_layers - 1
             in_features = hidden_size + input_size if layer_idx == 0 else hidden_size
             out_features = hidden_size * 3 if layer_idx == num_layers - 1 else hidden_size
             layers.append(nn.Linear(in_features, out_features, bias=not layer_norm))
-            if layer_norm and layer_idx != num_layers - 1:
+            if layer_norm:
                 layers.append(LayerNorm1d(out_features, affine=True))
+            if not last_layer:
                 layers.append(activation())
         self.net = nn.Sequential(*layers)
         self.hx_activation = self.activation()
+        self.cell_activation = self.activation()
 
     def forward(self, input, cell_state=None, reset_flags=None):
         if cell_state is None:
             cell_state = Variable(input.data.new(input.shape[1], self.hidden_size).zero_())
         else:
             cell_state = cell_state.squeeze(0)
+        if reset_flags is not None:
+            keep_flags = 1 - reset_flags
         outputs = []
         for step, x in enumerate(input):
             x = torch.cat([x, cell_state], 1)
             new_hx, new_cx, forget = self.net(x).chunk(3, 1)
             forget = sigmoid_pow(forget, 2)
+            retain = 1 - forget
             if reset_flags is not None:
-                forget = 1 - (1 - forget) * (1 - reset_flags[step].unsqueeze(-1)) # 1 - retain * keep_flags
-            cell_state = forget * new_cx + (1 - forget) * cell_state
+                retain *= keep_flags[step].unsqueeze(-1)
             new_hx = self.hx_activation(new_hx)
+            new_cx = self.cell_activation(new_cx)
+            cell_state = (1 - retain) * new_cx + retain * cell_state
             outputs.append(new_hx)
         return torch.stack(outputs, 0), cell_state.unsqueeze(0)
