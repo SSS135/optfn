a7f57a1678d92b3946c8ab2f6e85ae0d52eb8bc4
Alexander Penkin <sss13594@gmail.com>
2018-05-10 16:19:39 +0500

    somewhat working hqrnn

---
+++ opt_clip.py
@@ -0,0 +1,29 @@
+import torch
+import torch.autograd
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+
+class OptClipFunction(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x, pivot, clip):
+        assert x.shape == pivot.shape and clip.dim() == 0
+        ctx.save_for_backward(x, pivot, clip)
+        ctx.mark_dirty(x)
+        return x
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        x, pivot, clip = ctx.saved_tensors
+        grad_output = grad_output.clone()
+        diff = x - pivot
+        zero_mask = (diff > clip) & (grad_output < 0) | (diff < -clip) & (grad_output > 0)
+        grad_output[zero_mask] *= -0.1
+        return grad_output, None, None
+
+
+def opt_clip(x, pivot, clip):
+    if not isinstance(clip, torch.Tensor):
+        clip = torch.tensor(clip, dtype=x.dtype, device=x.device)
+    return OptClipFunction.apply(x, pivot, clip)
--- temporal_group_norm.py
+++ temporal_group_norm.py
@@ -1,7 +1,14 @@
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 class TemporalGroupNorm(nn.GroupNorm):
+    def forward(self, input):
+        x = input.view(input.shape[0] * input.shape[1], input.shape[2])
+        return super().forward(x).view_as(input)
+
+
+class TemporalLayerNorm(nn.LayerNorm):
     def forward(self, input):
         x = input.view(input.shape[0] * input.shape[1], input.shape[2])
         return super().forward(x).view_as(input)
