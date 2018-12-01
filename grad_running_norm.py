a802b5315e1ec1d4040a4260ebc7dd7cf9310954
Alexander Penkin <sss13594@gmail.com>
2018-12-01 21:24:23 +0500

    beta fix, mixture pd refactoring, v-trace

--- grad_running_norm.py
+++ grad_running_norm.py
@@ -10,18 +10,21 @@ class GradRunningNorm(nn.Module):
         super().__init__()
         self.momentum = momentum
         self.weight = weight
-        self.avg_sq = 0
-        self.step = 0
+        self._avg_sq = 0
+        self._step = 0
         self.register_backward_hook(self.backward_hook)
 
     def backward_hook(self, module, grad_input, grad_output):
-        assert len(grad_input) == 1, grad_input
         grad_input, = grad_input
-        self.avg_sq = self.momentum * self.avg_sq + (1 - self.momentum) * grad_input.detach().pow(2).sum().item()
-        self.step += 1
-        bias_correction = 1 - self.momentum ** self.step
-        norm = (self.avg_sq / bias_correction) ** 0.5
-        return grad_input * (self.weight / norm),
+        assert len(grad_input.shape) in (2, 3), grad_input.shape
 
-    def forward(self, input):
+        step_avg_sq = grad_input.detach().reshape(-1, grad_input.shape[-1]).pow(2).mean(0).sum().item() / (grad_input.shape[-1] ** 0.5)
+        self._avg_sq = self.momentum * self._avg_sq + (1 - self.momentum) * step_avg_sq
+
+        self._step += 1
+        bias_correction = 1 - self.momentum ** self._step
+        norm = (self._avg_sq / bias_correction) ** 0.5
+        return self.weight / norm * grad_input,
+
+    def forward(self, input: torch.Tensor):
         return input.clone()
