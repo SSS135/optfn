1730825cfb42b2c389af036eed063d884350a6b0
Alexander Penkin <sss13594@gmail.com>
2018-12-15 14:04:05 +0500

    improvements

--- iqn_loss.py
+++ iqn_loss.py
@@ -1,3 +1,5 @@
+from ppo_pytorch.common.barron_loss import barron_loss
+
 
 def l1_quantile_loss(output, target, tau, reduce=True):
     u = target - output
@@ -8,4 +10,10 @@ def l1_quantile_loss(output, target, tau, reduce=True):
 def huber_quantile_loss(output, target, tau, k=0.02, reduce=True):
     u = target - output
     loss = (tau - (u.detach() <= 0).float()).mul_(u.detach().abs().clamp(max=k).div_(k)).mul_(u)
+    return loss.mean() if reduce else loss
+
+
+def barron_quantile_loss(output, target, tau, alpha, c, reduce=True):
+    u = target - output
+    loss = (tau - (u.detach() <= 0).float()).abs() * barron_loss(output, target, alpha, c, False)
     return loss.mean() if reduce else loss
