6dfc4c4657aafd4e7e2b459570e97934a5f6cdc2
Alexander Penkin <sss13594@gmail.com>
2020-04-27 12:59:06 +0500

    add gradient logging to IMPALA

--- gradient_logger.py
+++ gradient_logger.py
@@ -4,30 +4,20 @@ import numpy as np
 from collections import defaultdict
 
 
-grad_mean = defaultdict(list)
-grad_square = defaultdict(list)
-log_interval = 100
-
-
 class GradientLoggerFunction(Function):
     @staticmethod
-    def forward(ctx, x, key):
-        ctx.key = key
+    def forward(ctx, x, logger, tag, step):
+        ctx.log_info = logger, tag, step
         return x
 
     @staticmethod
     def backward(ctx, grad_output):
-        mean = grad_mean[ctx.key]
-        square = grad_square[ctx.key]
-        mean.append(grad_output.mean().item())
-        square.append(grad_output.pow(2).mean().item())
-        if len(mean) >= log_interval:
-            std = (np.array(square) - np.array(mean) ** 2) ** 0.5
-            print(f'{ctx.key} grad rms {np.mean(square) ** 0.5:.6}, mean {np.mean(mean):.6}, std {np.mean(std):.6}')
-            mean.clear()
-            square.clear()
-        return grad_output, None
+        logger, tag, step = ctx.log_info
+        logger.add_scalar(f'Gradient/Mean {tag}', grad_output.mean(), step)
+        logger.add_scalar(f'Gradient/RMS {tag}', grad_output.pow(2).mean().sqrt(), step)
+        logger.add_histogram(f'Gradient {tag}', grad_output, step)
+        return grad_output, None, None, None
 
 
-def log_gradients(x: torch.Tensor, key: str):
-    return GradientLoggerFunction.apply(x, key) if x.requires_grad else x
+def log_gradients(x: torch.Tensor, logger, tag, step):
+    return GradientLoggerFunction.apply(x, logger, tag, step) if x.requires_grad else x
