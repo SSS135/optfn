48ae4f5439daa89ac54921a7642e612838c724eb
Alexander Penkin <sss13594@gmail.com>
2020-04-29 22:51:09 +0500

    move optfn

---
+++ setup.py
@@ -0,0 +1,18 @@
+import sys
+
+from setuptools import setup
+
+if sys.version_info.major != 3:
+    print("This Python is only compatible with Python 3, but you are running "
+          "Python {}. The installation will likely fail.".format(sys.version_info.major))
+
+setup(name='optfn',
+    install_requires=[
+    ],
+    description="optfn",
+    author="Alexander Penkin",
+    url='https://github.com/SSS135/optfn',
+    author_email="sss13594@gmail.com",
+    version="0.1",
+    packages=['optfn'],
+    zip_safe=False)
